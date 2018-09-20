2079b40559b6a21bfacf4ccec2f091e61428597b
JelleManders <jelle4life@gmail.com>
2018-09-20 14:00:48 +0200

    Intermediary commit, trying to figure out how to better implement cnjCombiner

--- jelle/Lab3.hs
+++ jelle/Lab3.hs
@@ -48,6 +48,8 @@ t3_2 = (Neg p)
 t3_3 = (Neg (Neg p))
 t3_4 = (Cnj [p,q])
 
+isConjNormFormTest = (Cnj [Dsj [(Neg p), (Equiv p q)], Dsj [r, p, (Neg p)], Dsj [r, (Neg q)]])
+
 isConjNormForm :: Form -> Bool
 isConjNormForm (Cnj listOfClauses) = and (map isClause listOfClauses)
 isConjNormForm f = isClause f || isLiteral f
@@ -60,4 +62,50 @@ isLiteral :: Form -> Bool
 isLiteral (Prop _) = True
 isLiteral (Neg (Prop _)) = True
 isLiteral _ = False
-    
+
+-- helper function to combine multiple conjunctions into one
+cnjCombiner :: Form -> [Form] -> Form
+cnjCombiner (Cnj acc) [] = (Cnj acc)
+cnjCombiner (Cnj acc) ((Cnj x):xs) = cnjCombiner (Cnj (acc ++ x)) xs 
+
+-- heper function that loops over all the forms in a list and packs all the literals in a Cnj
+cnjParse :: [Form] -> [Form]
+cnjParse f = map (\x -> if isLiteral x then (Cnj [x]) else x) f
+
+-- helper function that appllies the law of distribution of disjunction over its argument. Our argument f is of the form [Cnj [Form]]. If we take two possible indexes from the list, choose two conjunctions with list of formulas, then we can compose a list of disjunctions by choosing one formula from one conjunction list each and putting this in a Disjunction.
+dsjDistributor :: [Form] -> [Form]
+dsjDistributor unparsed_f = 
+    let f = cnjParse unparsed_f
+    in [ (Dsj [x,y]) | 
+    n <- [length f], i1 <- [0..n-1], i2 <- [i1+1 .. n-1],
+    (Cnj c1) <- [f !! i1], (Cnj c2) <- [f !! i2],
+    x <- c1, y <- c2 ]
+
+-- Convert into Conjunctive Normal Form. This function executes a single iteration of converter functions over the Formula.
+convertIntoCNF :: Form -> Form
+
+-- A singular property doesn't need to be converted, we can jsut return the argument
+convertIntoCNF (Prop x) = (Prop x)
+
+-- if we convert all elements of a conjunction, they must all be in conjunctive form. So if we combine the results of the conversion into one big conjunction, we will have converted the formula.
+convertIntoCNF (Cnj f) = cnjCombiner (Cnj []) (map convertIntoCNF f)
+
+-- the disjunction converter needs to convert all of its arguments, these will then be conjunctions. We are then in the form (Dsj [(Cnj [Form])]). From this we will have to apply the law of distribution of Disjunctions, our result is in CNF.
+convertIntoCNF (Dsj f) = (Cnj (dsjDistributor (map convertIntoCNF f)))
+
+-- converter for all negated formulas. If the negated Form is an atom, we are done
+convertIntoCNF (Neg (Prop x)) = (Neg (Prop x))
+
+-- the negation of a negation is itself again, we do need to convert the rest though
+convertIntoCNF (Neg (Neg x)) = convertIntoCNF x
+
+-- negation of a conjunction or disjunction requires us to distribute the negation over the formula in the list
+convertIntoCNF (Neg (Cnj fs)) = convertIntoCNF (Dsj (map (\x -> (Neg x)) fs))
+convertIntoCNF (Neg (Dsj fs)) = convertIntoCNF (Cnj (map (\x -> (Neg x)) fs))
+
+-- conversion of an implication is done by converting the equivalent formula: P -> Q = (-P v Q)
+convertIntoCNF (Impl f1 f2) = convertIntoCNF (Dsj [(Neg f1), f2])
+
+-- conversion of an equivalence is done by converting the disjunction of the conjunctives of negative and positive formulas
+convertIntoCNF (Equiv f1 f2) = convertIntoCNF (Dsj [(Cnj [f1, f2]), (Cnj [(Neg f1), (Neg f2)])])
+
