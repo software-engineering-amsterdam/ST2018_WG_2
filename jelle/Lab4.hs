6abb8f2a5247c6f6f3fb33e53848dc9f75b88025
JelleManders <jelle4life@gmail.com>
2018-09-26 16:39:00 +0200

    Inserted solution to question 8

--- jelle/Lab4.hs
+++ jelle/Lab4.hs
@@ -41,7 +41,7 @@ symClos ((x,y):xs) = sort $ nub ((x,y):(y,x):(symClos xs))
 
 
 -- ===============================
--- === 6: trClos Relations ==== 20 mins
+-- === 6: trClos Relations ==== 30 mins
 -- ===============================
 infixr 5 @@
 
@@ -55,3 +55,22 @@ trClosFunc a = nub $ sort (a ++ (a @@ a))
 -- the transitive closure of a set can be found by finding missing layers until adding a missing layer does not change the set, because if adding the next layer equals adding the empty set, we have no more missing items to insert into the set. If no items are missing, this means we are done.
 trClos :: Ord a => Rel a -> Rel a
 trClos a = fp' trClosFunc a
+
+-- ======================================
+-- === 8: sym(tr(R)) =?= tr (sym(R)) ====
+-- ======================================
+
+-- we can check this fairly easily by defining a function that makes use of quickCheck to find counterexamples. If the sym of the tr of any Rel is not the same as its tr of the sym, then we have found a counter example.
+symTrIsTrSymCheck :: [(Int, Int)] -> Bool
+symTrIsTrSymCheck a = (symClos $ trClos a) == (trClos $ symClos a)
+
+{-
+*Lab4> quickCheck symTrIsTrSymCheck
+*** Failed! Falsifiable (after 3 tests and 4 shrinks):    
+[(0,1)]
+
+quickCheck has found is a counterExample, the set [(0,1)]. This is a valid counterExample since the transitive closure of [(0,1)] is again [(0,1)] and its symmetric is [(0,1),(1,0)]. But, the transitive closure of [(0,1),(1,0)] is [(0,0),(0,1),(1,0),(1,1)]. This means that
+sym $ tr [(0,1)] = sym [(0,1),(1,0)] = [(0,0),(0,1),(1,0),(1,1)]
+tr $ sym [(0,1)] = tr [(0,1)]        = [(0,1),(1,0)]
+These two are not the same, hence a valid counterexample, hence there is a difference between the transitive of the symmetric and the symmetric of the transitive.
+-}
